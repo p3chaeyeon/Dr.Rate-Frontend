aa8b0fc7992a2a119fea0e6eb6fa8fd77884ee46
p3chaeyeon <p3chaeyeon@gmail.com>
2024-11-19 21:39:16 +0900

    feat: 웹사이트 <footer> 완성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,84 @@
 import styles from './Footer.module.scss';
 import React from 'react';
+import footerDivider from 'src/assets/icons/footerDivider.svg';
+import faviconIcon from 'src/assets/icons/faviconIcon.svg';
+import labIcon from 'src/assets/icons/labIcon.svg';
+import appstoreIcon from 'src/assets/icons/appstoreIcon.svg';
+import playstoreIcon from 'src/assets/icons/playstoreIcon.svg';
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <div className={styles.footerContainer}>
+                <div className={ styles.footerInfo }>
+                    <div className={ styles.corName }>
+                        (주)금리박사
+                    </div> {/* // <div className={ styles.corName }> */}
+
+                    <div className={ styles.corAddress}>
+                        <div>
+                            <p className={ styles.firstRow }><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></p>
+                            <p><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></p>
+                        </div>
+                        <div className={styles.addressTitle}>
+                            <div className={ styles.firstRow }>
+                                {/* 본사 */}
+                                <span>본</span>
+                                <span className={styles.noneSpan}>박</span> {/* 가리는 텍스트 */}
+                                <span className={styles.noneSpan}>박</span>
+                                <span className={styles.noneSpan}>박</span>
+                                <span>사</span>
+                            </div>
+                            <div>
+                                {/* 금리연구소 */}
+                                <span>금</span>
+                                <span>리</span>
+                                <span>연</span>
+                                <span>구</span>
+                                <span>소</span>
+                            </div>
+                        </div>
+                        <div>
+                            <p className={ styles.firstRow }><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></p>
+                            <p><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></p>
+                        </div>
+                        <div>
+                            <p className={ styles.firstRow }>서울 강남구 강남대로 94길 20 삼오빌딩 5-9층</p>
+                            <p>서울 강남구 강남대로 94길 20 삼오빌딩 602호</p>
+                        </div>
+                    </div>{/* // <div className={ styles.corAddress}> */}
+
+                    <div className={ styles.corContact }>
+                        <div>
+                            <div className={ styles.contactTitle }>TEL.</div>
+                            <div className={ styles.contactContent }>0507-1414-9601</div>
+                        </div>
+                        <div><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></div>
+                        <div>
+                            <div className={ styles.contactTitle }>E-mail.</div>
+                            <div className={ styles.contactContent }>paul9119298@gmail.com</div>
+                        </div>
+                        <div><img src={footerDivider} alt="푸터 세로 구분선" className={styles.footerDivider} /></div>
+                        <div>
+                            <div className={ styles.contactTitle }>H.P.</div>
+                            <div className={ styles.contactContent }>http://localhost:5173</div>
+                        </div>
+                    </div>{/* // <div className={ styles.corContact }> */}
+
+                    <div className={ styles.corRight }>
+                        <div className={ styles.rightText }>COPYRIGHT</div>
+                        <div className={ styles.rightText }>DR.RATE.,LTD & Dancingtuna</div>
+                        <div className={ styles.rightText }>RIGHTS RESERVED.</div>
+                    </div>{/* // <div className={ styles.corRight }> */}
+                </div> {/* // <div className={ styles.footerInfo }> */}
+
+                <div className={ styles.footerIcon }>
+                    <div><img src={faviconIcon} alt="Footer Icon" className={ styles.iconSvg } /></div>
+                    <div><img src={labIcon} alt="Footer Icon" className={ styles.iconSvg } /></div>
+                    <div><img src={appstoreIcon} alt="Footer Icon" className={ styles.iconSvg } /></div>
+                    <div><img src={playstoreIcon} alt="Footer Icon" className={ styles.iconSvg } /></div>
+                </div>
+            </div>
         </footer>
     );
 };
