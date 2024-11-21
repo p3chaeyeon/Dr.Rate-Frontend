c87247e4b221217739fac237783ceb95ab314a0a
p3chaeyeon <p3chaeyeon@gmail.com>
2024-11-21 17:11:30 +0900

    design : Admin Logo 추가, 사용자 헤더 로고 속성 수정

--- src/components/AdminSideNav/AdminSideNav.jsx
+++ src/components/AdminSideNav/AdminSideNav.jsx
@@ -2,16 +2,16 @@ import styles from './AdminSideNav.module.scss';
 import React from 'react';
 import { PATH } from "src/utils/path";
 import { useNavigate } from "react-router-dom";
+import adminLogo from 'src/assets/images/adminLogo.png';
 
 const AdminSideNav = () => {
     const navigate = useNavigate();
 
     return (
         <div className={ styles.adminNav }>
-            <div className={ styles.adminLogo }>
-                {/* 여기 로고 넣기 */}
+            <div className={ styles.adminLogoDiv }  onClick={() => navigate(PATH.ADMIN_MAIN)}>
+                <img src={adminLogo} alt="admin logo" className={styles.adminLogo} />
             </div>
-
 
             <div className={ styles.adminMenu }>
                 {/* 여기에 관리자 메뉴 */}
