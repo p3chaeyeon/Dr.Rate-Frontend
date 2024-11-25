bbd85dcb2260332bea164625b3247e6b556526fa
윤성 <yunseong0220@hanmail.net>
2024-11-25 12:28:07 +0900

    feat : 모달추가

--- src/components/Modal/AlertModal/AlertModal.jsx
+++ src/components/Modal/AlertModal/AlertModal.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useAtom } from 'jotai';
-import { isAlertOpenAtom, alertContentAtom } from '../atoms/alertAtom';
+import { isAlertOpenAtom, alertContentAtom } from 'src/atoms/alertAtom';
+
 import styles from './AlertModal.module.scss';
 
 const AlertModal = () => {
-  const [isAlertOpen, setIsAlertOpen] = useAtom(isAlertOpenAtom);
-  const [alertContent] = useAtom(alertContentAtom);
+  const [isAlertOpen, setIsAlertOpen] = useAtom(isAlertOpenAtom); //모달 열림 상태 및 상태변경함수
+  const [alertContent] = useAtom(alertContentAtom); //모달의 제목과 메시지 상태
 
   if (!isAlertOpen) return null; // 모달이 닫혀있으면 렌더링하지 않음
 
-  const handleClose = () => setIsAlertOpen(false); // 모달 닫기
+  const handleClose = () => setIsAlertOpen(false); // 확인 버튼 클릭 시 모달 닫기
 
   return (
     <div className={styles.alertModal}>
