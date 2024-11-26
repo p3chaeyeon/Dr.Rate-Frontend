93ff4e9c1bd1ff1dfffa447a076f3aa8d30c3e77
p3chaeyeon <p3chaeyeon@gmail.com>
2024-11-26 17:03:45 +0900

    feat : AdminSideNav icon 크기 조정, hover 영역 조정, 경로에 따라 폰트 굵기, 크기 조정

--- src/components/AdminSideNav/AdminSideNav.jsx
+++ src/components/AdminSideNav/AdminSideNav.jsx
@@ -1,7 +1,7 @@
 import styles from './AdminSideNav.module.scss';
 import React from 'react';
 import { PATH } from "src/utils/path";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import adminLogo from 'src/assets/images/adminLogo.png';
 import dashboard from 'src/assets/images/adminDashboard.png';
 import userManagement from 'src/assets/images/adminUserManagement.png';
@@ -9,6 +9,9 @@ import inquiryHistory from 'src/assets/images/adminInquiryHistory.png';
 
 const AdminSideNav = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const isPathActive = (paths) => paths.some((path) => location.pathname.includes(path));
+
 
     return (
         <div className={ styles.adminNav }>
@@ -20,12 +23,18 @@ const AdminSideNav = () => {
 
             {/* 관리자 메뉴 */}
             <div className={ styles.adminMenuDiv }>
-                <div>
+                {/* <div> */}
                     <div className={ styles.adminMenuList } onClick={()=>navigate(PATH.ADMIN_MAIN)}> 
                         <div className={ styles.adminMenuItemImg}>
                             <img src={dashboard} alt="대시보드" className={styles.adminMenuImg} />
                         </div>
-                        <div className={ styles.adminMenuItemText }>
+                        <div 
+                            className={ styles.adminMenuItemText }
+                            style={{
+                                fontWeight: location.pathname.includes(PATH.ADMIN_MAIN) ? '700' : 'inherit',
+                                fontSize : location.pathname.includes(PATH.ADMIN_MAIN) ? '18px' : 'inherit',
+                            }} 
+                        >
                             대시보드
                         </div>
                     </div>
@@ -33,7 +42,13 @@ const AdminSideNav = () => {
                         <div className={ styles.adminMenuItemImg}>
                             <img src={userManagement} alt="사용자 조회" className={styles.adminMenuImg} />
                         </div>
-                        <div className={ styles.adminMenuItemText }>
+                        <div 
+                            className={ styles.adminMenuItemText }
+                            style={{
+                                fontWeight: location.pathname.includes(PATH.ADMIN_USER_LIST) ? '700' : 'inherit',
+                                fontSize : location.pathname.includes(PATH.ADMIN_USER_LIST) ? '18px' : 'inherit',
+                            }} 
+                        >
                             사용자 조회
                         </div>                        
                     </div>
@@ -41,11 +56,17 @@ const AdminSideNav = () => {
                         <div className={ styles.adminMenuItemImg}>
                             <img src={inquiryHistory} alt="1:1 문의 내역" className={styles.adminMenuImg} />
                         </div>
-                        <div className={ styles.adminMenuItemText }>
+                        <div 
+                            className={ styles.adminMenuItemText }
+                            style={{
+                                fontWeight: isPathActive([PATH.ADMIN_INQUIRE_LIST, PATH.ADMIN_INQUIRE]) ? '700' : 'inherit',
+                                fontSize : isPathActive([PATH.ADMIN_INQUIRE_LIST, PATH.ADMIN_INQUIRE]) ? '18px' : 'inherit',
+                            }}  
+                        >
                             1:1 문의 내역
                         </div>
                     </div>
-                </div>
+                {/* </div> */}
             </div>{/* //<div className={ styles.adminMenuDiv }> */}
 
 
