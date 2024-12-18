92b66ff388ac6d2bec3080ee615d81a322771238
p3chaeyeon <p3chaeyeon@gmail.com>
2024-12-18 10:23:10 +0900

    fix : 토큰 변경경

--- src/apis/favoriteAPI.js
+++ src/apis/favoriteAPI.js
@@ -7,7 +7,7 @@ import { PATH } from 'src/utils/path';
 const getSessionToken = () => {
   // return localStorage.getItem('sessionToken'); 
   // 그냥 토큰키 넣어서 함.
-  return "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OCwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM0Mzk3NTY1LCJleHAiOjE3MzQ0ODM5NjV9.EXFCRgASw3q09351HyDkJKFrWBWfNvjcOY1FGtMRaiA";
+  return "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OCwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzM0NDg0NTUyLCJleHAiOjE3MzQ1NzA5NTJ9.hTBolZi4LYfZ9vwOxVtxOjQW0ZNthnUEh9kwDCCORpA";
 };
 
 
