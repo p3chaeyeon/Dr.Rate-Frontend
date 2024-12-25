4fcdb71ed3c521640a9221902d9cb6b0ef111b17
p3chaeyeon <p3chaeyeon@gmail.com>
2024-12-25 21:06:16 +0900

    fix : Authorization 'undefined' 일 때 isLoggedIn false 처리

--- src/atoms/sessionAtom.js
+++ src/atoms/sessionAtom.js
@@ -3,9 +3,9 @@
 import { atom } from 'jotai';
 
 // Authorization 키가 로컬 스토리지에 있는지 확인하여 초기값 설정
-const isLoggedInAtom = atom(!!localStorage.getItem('Authorization'));
+const isLoggedInAtom = atom(() => {
+  const savedToken = localStorage.getItem('Authorization');
+  return savedToken && savedToken !== 'undefined'; // 저장된 토큰이 있고 'undefined'가 아닌 경우 true
+});
 
 export { isLoggedInAtom };
-
-
-
